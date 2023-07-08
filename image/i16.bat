fedd97218f85e34d45802744ee92bbb014d1ce8a
sashi0034 <satoshi3104.nishi@gmail.com>
2023-07-08 17:56:06 +0900

    タイルデータを別ファイルに分離

--- image/i16.bat
+++ image/i16.bat
@@ -1,20 +1,21 @@
 @echo on
-set file=%1
+set FILE=%1
 
 if not exist output (
     mkdir output
 ) 
+set FILE=%1
+
+set FILE_UPPER=%1
+for %%i in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do call ^
+    set FILE_UPPER=%%FILE_UPPER:%%i=%%i%%
+echo %FILE_UPPER%
 
 .\img2gb\img2gb-1.1.0.exe tileset ^
-    --output-c-file="output\%file%.set.c" ^
-    --output-header-file="output\%file%.set.h" ^
-    --output-image="output\%file%.gb.png" ^
+    --output-c-file="..\tileset_%FILE%.c" ^
+    --output-header-file="..\tileset_%FILE%.h" ^
+    --output-image="output\%FILE%.gb.png" ^
     --sprite8x16 ^
     --alternative-palette ^
-    .\%file%.png
-
-@REM .\img2gb\img2gb-1.1.0.exe tilemap ^
-@REM     --output-c-file="output\%file%.map.c" ^
-@REM     --output-header-file="output\%file%.map.h" ^
-@REM     "output\%file%.gb.png" ^
-@REM     .\%file%.png
+    --name "TILESET_%FILE_UPPER%" ^
+    .\%FILE%.png

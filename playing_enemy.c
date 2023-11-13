130c2f25527f111cade0312959803e2fd76dfbd7
sashi0034 <satoshi3104.nishi@gmail.com>
2023-11-13 10:09:17 +0900

    4方向からポップする仮実装

--- playing_enemy.c
+++ playing_enemy.c
@@ -6,6 +6,8 @@ typedef struct {
     UINT8 spr_base;
     UINT8 anim_timer;
     UINT8 anim_frame;
+    UINT8 dir;
+    UINT8 remaining_move;
     MovableTrait movable;
 } Enemy;
 
@@ -16,7 +18,7 @@ typedef struct {
 
 EnemyManager s_enemy_manager;
 
-void fill_enemy_tiles(UINT8 base_spr, UINT8 base_u, BOOL is_flip) {
+static void fill_enemy_tiles(UINT8 base_spr, UINT8 base_u, BOOL is_flip) {
     if (is_flip) {
         set_sprite_prop(base_spr + 0, S_PALETTE | S_FLIPX);
         set_sprite_prop(base_spr + 1, S_PALETTE | S_FLIPX);
@@ -30,7 +32,7 @@ void fill_enemy_tiles(UINT8 base_spr, UINT8 base_u, BOOL is_flip) {
     }
 }
 
-void locate_enemy(UINT8 base_spr, UINT8 x, UINT8 y) {
+static void locate_enemy(UINT8 base_spr, UINT8 x, UINT8 y) {
     x += SPR_GAP_X;
     y += SPR_GAP_Y - 16 - AREA_PADDING_Y;
 
@@ -38,27 +40,75 @@ void locate_enemy(UINT8 base_spr, UINT8 x, UINT8 y) {
     move_sprite(base_spr + 1, x + 8, y);
 }
 
+static void set_enemy_move_delta(Enemy *enemy) {
+    switch (enemy->dir) {
+    case DIR_RIGHT:
+        enemy->movable.dx = 32;
+        break;
+    case DIR_LEFT:
+        enemy->movable.dx = -32;
+        break;
+    case DIR_DOWN:
+        enemy->movable.dy = 32;
+        break;
+    case DIR_UP:
+        enemy->movable.dy = -32;
+        break;
+    default:
+        break;
+    }
+}
+
 void update_enemy(Enemy *enemy) {
     if (enemy->is_using == FALSE)
         return;
 
-    // 移動処理
     if (move_movable(&enemy->movable)) {
+        // 移動処理
         locate_enemy(enemy->spr_base, enemy->movable.x, enemy->movable.y);
-        // 止まったら解放
     } else if (is_movable_stopped(&(enemy->movable))) {
-        enemy->is_using = FALSE;
-        return;
+        // 止まったら解放
+        enemy->remaining_move--;
+        if (enemy->remaining_move > 0) {
+            set_enemy_move_delta(enemy);
+        } else {
+            enemy->is_using = FALSE;
+        }
     }
 }
 
-void pop_enemy(Enemy *enemy) {
+static void pop_enemy(Enemy *enemy) {
+    UINT8 dir;
     enemy->is_using = TRUE;
 
     reset_movable(&enemy->movable, 1, 2);
-    enemy->movable.x = -16;
-    enemy->movable.y = 32 + MUL_P2(get_rand() & 3, 32);
-    enemy->movable.dx = 96;
+
+    dir = get_rand() & 3;
+    enemy->dir = dir;
+    switch (dir) {
+    case DIR_UP:
+        enemy->remaining_move = 3;
+        enemy->movable.x = 32 + MUL_P2(get_rand() & 3, 32);
+        enemy->movable.y = SCH_144;
+        break;
+    case DIR_DOWN:
+        enemy->remaining_move = 3;
+        enemy->movable.x = 32 + MUL_P2(get_rand() & 3, 32);
+        enemy->movable.y = -8;
+        break;
+    case DIR_RIGHT:
+        enemy->remaining_move = 7;
+        enemy->movable.x = -24;
+        enemy->movable.y = 32 + MUL_P2(get_rand() & 3, 32);
+        break;
+    case DIR_LEFT:
+        enemy->remaining_move = 7;
+        enemy->movable.x = SCW_160;
+        enemy->movable.y = 32 + MUL_P2(get_rand() & 3, 32);
+        break;
+    default:
+        break;
+    }
 
     locate_enemy(enemy->spr_base, enemy->movable.x, enemy->movable.y);
     fill_enemy_tiles(enemy->spr_base, 0, FALSE);
